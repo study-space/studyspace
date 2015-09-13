1ec5887994a1239222d39d268a4274ed77da62d6
SejongPark <devsejong@gmail.com>
2015-09-14 08:48:31 +0900

    Lombok 적용

--- src/main/java/io/github/studyspace/model/Place.java
+++ src/main/java/io/github/studyspace/model/Place.java
@@ -1,5 +1,8 @@
 package io.github.studyspace.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -13,36 +16,19 @@ import java.io.Serializable;
  */
 @Entity
 @Table(name = "PLACE")
+@Data @NoArgsConstructor
 public class Place implements Serializable {
-
-    private static final long serialVersionUID = -2850734990124055219L;
-
-    @Id
-    @GeneratedValue
-    // ID
-    private long id;
-
-    // 장소명
+    private @Id @GeneratedValue long id;
     private String name;
-
+    private String address;
+    private String telephoneNumber;
+    private String url;
     @Embedded
     private Position position;
 
-    private String address;
-
-    // 전화번호
-    private String telephoneNumber;
-
-    private String url;
-
-    // 거리
     // TODO DTO로 분리. 두객체의 연관관계에서만 나오는 값이다 이 값은 객체의 상태가 되어서는 안된다.
     @Transient
     private double distance;
-
-    public Place() {
-
-    }
 
     public Place(String name, Position position, String telephoneNumber, String address, String url) {
         this.name = name;
@@ -50,75 +36,5 @@ public class Place implements Serializable {
         this.position = position;
         this.telephoneNumber = telephoneNumber;
         this.url = url;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-
-    public Position getPosition() {
-       return position;
-    }
-
-    public void setPosition(Position position) {
-        this.position = position;
-    }
-
-    public String getTelephoneNumber() {
-        return telephoneNumber;
-    }
-
-    public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber = telephoneNumber;
-    }
-
-    public double getDistance() {
-        return distance;
-    }
-
-    public void setDistance(double distance) {
-        this.distance = distance;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Place{");
-        sb.append("id=").append(id);
-        sb.append(", name='").append(name).append('\'');
-        sb.append(", address='").append(address).append('\'');
-        sb.append(", position=").append(this.getPosition());
-        sb.append(", telephoneNumber='").append(telephoneNumber).append('\'');
-        sb.append(", distance=").append(distance);
-        sb.append('}');
-        return sb.toString();
     }
 }
--- src/main/java/io/github/studyspace/model/Position.java
+++ src/main/java/io/github/studyspace/model/Position.java
@@ -1,47 +1,23 @@
 package io.github.studyspace.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 
 //Value Object
 @Embeddable
+@Data
+@NoArgsConstructor
 public class Position {
     //위도
-
     private double longitude;
     //경도
     private double latitude;
 
-    public Position(){
-
-    }
 
     public Position(double latitude, double longitude) {
         this.longitude = longitude;
         this.latitude = latitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Location{");
-        sb.append("longitude=").append(longitude);
-        sb.append(", latitude=").append(latitude);
-        sb.append('}');
-        return sb.toString();
     }
 }

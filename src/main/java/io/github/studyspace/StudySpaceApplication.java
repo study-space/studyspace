d9866e62c5e8d0ab01e4d6bf4f780675ad53e6f1
evilimp <cuteimp@gmail.com>
2015-09-13 23:45:15 +0900

    Recreate Class file

---
+++ src/main/java/io/github/studyspace/StudySpaceApplication.java
@@ -0,0 +1,12 @@
+package io.github.studyspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpaceApplication.class, args);
+    }
+}

f76bb48762c6396762d0871d7245370c218201aa
evilimp <cuteimp@gmail.com>
2015-09-20 22:43:12 +0900

    add google analytics script change spring boot application configuration

--- src/main/java/io/github/studyspace/StudySpaceApplication.java
+++ src/main/java/io/github/studyspace/StudySpaceApplication.java
@@ -2,18 +2,10 @@ package io.github.studyspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"io.github.studyspace"})
 @SpringBootApplication
 public class StudySpaceApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(StudySpaceApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(StudySpaceApplication.class, args);
